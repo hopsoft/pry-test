88b97c74e789909dc0f35dcc61be1ba8c56671a5
Nathan Hopkins <natehop@gmail.com>
2012-11-06 17:31:15 -0700

    Added docs to the test class.

--- lib/micro_test/test.rb
+++ lib/micro_test/test.rb
@@ -2,66 +2,54 @@ require "observer"
 require "thread"
 
 module MicroTest
-  class TestCell
-    include Celluloid
 
-    attr_reader :test_class, :desc, :asserts, :duration
-
-    def initialize(test_class, desc, &block)
-      @test_class = test_class
-      @desc = desc
-      @test = block
-    end
-
-    def invoke(formatter)
-      @formatter = formatter
-      start = Time.now
-      @test.call
-      @duration = Time.now - start
-      @asserts = @test_class.asserts[desc]
-      @formatter.test_finished self
-    end
-
-    def finished?
-      !duration.nil?
-    end
-
-    def passing?
-      return false unless asserts
-      asserts.map{ |a| !!a[:value] }.uniq == [true]
-    end
-
-    def reset
-      @asserts = nil
-      @duration = nil
-    end
-
-  end
-
+  # Superclass for all test classes.
+  # @example Create a subclass with a test.
+  #   class SimpleTest < MicroTest::Test
+  #     test "common sense" do
+  #       assert 1 > 0
+  #     end
+  #   end
   class Test
     MUTEX = Mutex.new
 
     class << self
 
+      # All subclasses of this class.
+      # @note Primarily used in the context of MicroTest::Test to iterate over
+      #   the test classes. Deeper test class inhertance is simply ignored when
+      #   running tests.
+      # @return [Array<MicroTest::Test>]
       def subclasses
         @subclasses ||= []
       end
 
+      # All individual tests defined in this class.
+      # Used by subclasses of MicroTest::Test to hold tests defined
+      # by the MicroTest::Test.test class method.
+      # @return [Array<MicroTest::TestWrapper>]
       def tests
         @tests ||= []
       end
 
+      # All asserts that have been invoked.
+      # Each subclass of MicroTest::Test tracks all invoked asserts.
+      # Asserts are invoked within tests during test runs.
+      # @return [Hash]
       def asserts
         @asserts ||= {}
       end
 
+      # All files that define subclasses of this class.
+      # @note Primarily used in the context of MicroTest::Test.
+      # @example Files are stored in a Hash with the following format.
+      #   {
+      #     "path/to/file1.rb" => ["line 1", "line 2", "line 3", ...],
+      #     "path/to/file2.rb" => ["line 1", "line 2", "line 3", ...]
+      #   }
+      # @return [Hash]
       def files
         @files ||= {}
-      end
-
-      # Holds the state for all performed asserts for this class.
-      def asserts
-        @asserts ||= {}
       end
 
       # Resets the state in preparation for a new test run.
@@ -71,16 +59,39 @@ module MicroTest
         tests.each { |test| test.reset }
       end
 
+      # A callback provided by Ruby that is invoked whenever a subclass is created.
       def inherited(subclass)
-        file_path = caller_path(caller)
+        file_path = path(caller, 0)
         files[file_path] = File.open(file_path).readlines
         subclasses << subclass
       end
 
+      # Defines a test.
+      # Allows subclasses to define tests in their class definition.
+      #
+      # @param [String] desc A description for the test.
+      # @yield A block that defines the test code.
+      #
+      # @example
+      #   class SimpleTest < MicroTest::Test
+      #     test "common sense" do
+      #       assert 1 > 0
+      #     end
+      #   end
       def test(desc, &block)
-        tests << TestCell.new(self, desc, &block)
+        tests << MicroTest::TestWrapper.new(self, desc, &block)
       end
 
+      # A basic assert method to be used within tests.
+      #
+      # @param [Object] value The value to assert.
+      #
+      # @example
+      #   class SimpleTest < MicroTest::Test
+      #     test "common sense" do
+      #       assert 1 > 0
+      #     end
+      #   end
       def assert(value)
         info = assert_info(caller)
         key = info[:test_desc]
@@ -89,8 +100,13 @@ module MicroTest
           asserts[key] ||= []
           asserts[key] << info.merge(:value => value)
         end
+
+        value
       end
 
+      # Indicates if all tests have finished running.
+      # @note Used when running tests in async mode.
+      # @return [Boolean]
       def finished?
         if tests.empty?
           results = [true]
@@ -104,10 +120,23 @@ module MicroTest
 
       private
 
+      # Builds a Hash of assert information for the given call stack.
+      #
+      # @param [Array<String>] stack The call stack to extract info from.
+      #
+      # @example
+      #   {
+      #     :file_path => "/path/to/test_file.rb",
+      #     :line_num => 100,
+      #     :line => "  assert 'something' do",
+      #     :test_desc => 'the test description'
+      #   }
+      #
+      # @return [Hash]
       def assert_info(stack)
-        file_path = caller_path(stack)
+        file_path = path(stack, 0)
         lines = MicroTest::Test.files[file_path]
-        line_num = caller_line(stack)
+        line_num = line_number(stack, 0)
         line_index = line_num - 1
         line = lines[line_index]
         test_desc = test_desc(lines, line_index)
@@ -119,17 +148,31 @@ module MicroTest
         }
       end
 
-      def caller_path(stack)
-        stack[0][0, stack[0].index(/:[0-9]+:/)]
+      # Returns a file path from a call stack.
+      # @param [Array<String>] stack The call stack to pull a path from.
+      # @param [Integer] index The index of the call stack entry to use.
+      # @return [String]
+      def path(stack, index)
+        stack[index][0, stack[index].index(/:[0-9]+:/)]
       end
 
-      def caller_line(stack)
-        stack[0].scan(/:[0-9]+:/).first.gsub(/:/, "").to_i
+      # Returns a line number from a call stack.
+      # @param [Array<String>] stack The call stack to pull a path from.
+      # @param [Integer] index The index of the call stack entry to use.
+      # @return [String]
+      def line_number(stack, index)
+        stack[index].scan(/:[0-9]+:/).first.gsub(/:/, "").to_i
       end
 
+      # Finds the test description within a list of lines.
+      # The lines are typically the lines read from a file.
+      # @param [Array<String>] lines The lines to find the description in.
+      # @param [Integer] start The starting index for the search.
+      # @return [String]
       def test_desc(lines, start)
         reversed = lines[0..start].reverse
         index = reversed.index { |line| line =~ /\Wtest\s/ }
+        return "unknown test" unless index
         line = reversed[index]
         line.gsub(/\Wtest\s|\sdo\W|'|"/, "").strip
       end
