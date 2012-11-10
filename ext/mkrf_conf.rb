d5680b627c11f056d2033c11161b8a632c6635a6
Nathan Hopkins <natehop@gmail.com>
2012-11-10 08:08:49 -0700

    Added ext dir that I missed last commit.

---
+++ ext/mkrf_conf.rb
@@ -0,0 +1,33 @@
+# based on instructions here:
+# http://en.wikibooks.org/wiki/Ruby_Programming/RubyGems#How_to_install_different_versions_of_gems_depending_on_which_version_of_ruby_the_installee_is_using
+require 'rubygems'
+require 'rubygems/command.rb'
+require 'rubygems/dependency_installer.rb'
+
+begin
+  Gem::Command.build_args = ARGV
+  rescue NoMethodError
+end
+
+installer = Gem::DependencyInstaller.new
+
+begin
+
+  if RUBY_PLATFORM != "java"
+    puts "Installing pry and pry-stack_explorer."
+    installer.install "pry"
+    installer.install "pry-stack_explorer"
+  else
+    puts "Platform is java... skip install for pry and pry-stack_explorer."
+  end
+rescue Exception => e
+  puts e.message
+  puts e.backtrace.join("\n")
+  exit(1)
+end
+
+f = File.open(File.join(File.dirname(__FILE__), "Rakefile"), "w")   # create dummy rakefile to indicate success
+f.write("task :default\n")
+f.close
+
+exit
