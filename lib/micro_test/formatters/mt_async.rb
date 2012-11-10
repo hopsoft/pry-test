2fa718963cf25ad5af0688870973feb89174bd91
Nathan Hopkins <natehop@gmail.com>
2012-11-10 16:29:02 -0700

    Added the default async formatter.

--- lib/micro_test/formatters/mt_async.rb
+++ lib/micro_test/formatters/mt_async.rb
@@ -9,8 +9,33 @@ module MicroTest
 
     def after_suite(test_classes)
       puts
+
+      test_classes.each do |test_class|
+        puts
+        puts test_class.name.ljust(80, "-")
+
+        test_class.tests.each do |test|
+          duration = (test.duration * 10**4).round.to_f / 10**4
+          print yellow("  #{duration.to_s.ljust(6, "0")}")
+
+          if test.passed?
+            print green(" #{test.desc}")
+          else
+            print red(" #{test.desc}")
+          end
+
+          puts
+        end
+      end
+
+      puts
       puts "".ljust(80, "-")
-      puts "Finished in #{duration} seconds."
+      print " #{passed + failed} Tests finished in #{yellow duration} seconds. "
+      totals = []
+      totals << green("#{passed} Passed") if passed > 0
+      totals << red("#{failed} Failed") if failed > 0
+      print "(#{totals.join(", ")})"
+      puts
       puts
     end
 
