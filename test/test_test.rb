5b548ef0939af77036b479b6c28e4c37e1cb1b8c
Nathan Hopkins <natehop@gmail.com>
2012-11-10 21:24:17 -0700

    Added tests for files.

--- test/test_test.rb
+++ test/test_test.rb
@@ -1,6 +1,5 @@
 class TestTest < MicroTest::Test
 
-
   test "properly stores subclasses" do
     assert MicroTest::Test.subclasses.is_a?(Array)
     assert MicroTest::Test.subclasses.length > 0
@@ -11,6 +10,13 @@ class TestTest < MicroTest::Test
     assert MicroTest::Test.tests.is_a?(Array)
     assert MicroTest::Test.subclasses.first.tests.length > 0
     assert MicroTest::Test.subclasses.first.tests.first.is_a?(MicroTest::TestWrapper)
+  end
+
+  test "properly stores files" do
+    assert MicroTest::Test.files.is_a?(Hash)
+    assert MicroTest::Test.files.has_key?(__FILE__)
+    assert MicroTest::Test.files[__FILE__].is_a?(Array)
+    assert MicroTest::Test.files[__FILE__].select{ |l| l.start_with?("    assert MicroTest::Test.files[__FILE__].select{ |l| l.start_with?(") }.length > 0
   end
 
 
