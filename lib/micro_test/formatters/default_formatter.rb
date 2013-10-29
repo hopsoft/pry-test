4685a80ae32c85363589f99ccfdd4f5d322c55dc
Nathan Hopkins <natehop@gmail.com>
2013-10-29 15:04:25 -0600

    Broke some methods out of the default formatter.

--- lib/micro_test/formatters/default_formatter.rb
+++ lib/micro_test/formatters/default_formatter.rb
@@ -11,39 +11,13 @@ module MicroTest
 
     def after_test(test)
       duration = (test.duration * 10**4).round.to_f / 10**4
-      if duration < 0.01
-        print yellow("  #{duration.to_s.ljust(6, "0")}")
-      else
-        print red("  #{duration.to_s.ljust(6, "0")}")
-      end
+      color = duration < 0.01 ? :yellow : :red
+      print send(color, "  #{duration.to_s.ljust(6, "0")}")
 
       if test.passed?
         print green(" #{test.desc}")
       else
-        puts red(" #{test.desc}")
-        test.failed_asserts.each do |assert|
-          puts
-          print "".ljust(9)
-          puts "#{assert[:file_path]}:#{red(assert[:line_num])}"
-          puts "".ljust(9) + "".rjust(71, "-")
-          index = assert[:line_num] - 1
-          start = index - 2
-          start = 0 if start <= 0
-          finish = index + 2
-          finish = assert[:lines].length - 1 if finish >= assert[:lines].length
-          (start..finish).each do |i|
-            print "".ljust(9)
-            if i == index
-              print red((i + 1).to_s.rjust(3, "0"))
-              print red("|")
-              print red(assert[:lines][i])
-            else
-              print (i + 1).to_s.rjust(3, "0")
-              print "|"
-              print assert[:lines][i]
-            end
-          end
-        end
+        print_test_failure test
       end
 
       puts
@@ -59,6 +33,43 @@ module MicroTest
       print "(#{totals.join(", ")})"
       puts
       puts
+    end
+
+    private
+
+    def print_test_failure(test)
+      puts red(" #{test.desc}")
+      test.failed_asserts.each do |assert|
+        print_assert_failure assert
+      end
+    end
+
+    def print_assert_failure(assert)
+      puts
+      print "".ljust(9)
+      puts "#{assert[:file_path]}:#{red(assert[:line_num])}"
+      puts "".ljust(9) + "".rjust(71, "-")
+      index = assert[:line_num] - 1
+      start = index - 2
+      start = 0 if start <= 0
+      finish = index + 2
+      finish = assert[:lines].length - 1 if finish >= assert[:lines].length
+      print_assert_failure_lines assert, start, finish
+    end
+
+    def print_assert_failure_lines(assert, start, finish)
+      (start..finish).each do |i|
+        print "".ljust(9)
+        if i == index
+          print red((i + 1).to_s.rjust(3, "0"))
+          print red("|")
+          print red(assert[:lines][i])
+        else
+          print (i + 1).to_s.rjust(3, "0")
+          print "|"
+          print assert[:lines][i]
+        end
+      end
     end
 
   end
