082a4ed8678c57e848bc95b9a0d88ec2b024aad1
Nathan Hopkins <natehop@gmail.com>
2014-11-29 10:56:06 -0700

    Moved coveralls init so it picks up all files.

--- Rakefile
+++ Rakefile
@@ -4,6 +4,7 @@ task :default => [:test]
 
 desc "Runs the test suite."
 task :test do
-  exec File.expand_path("../bin/pry-test --disable-pry", __FILE__)
+  env = ENV.to_hash.merge("PRY_TEST_COVERAGE" => "true")
+  exec env, File.expand_path("../bin/pry-test --disable-pry", __FILE__)
 end
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,1 @@
-if ENV["PRY_TEST_DEMO"] != "true"
-  require "simplecov"
-  require "coveralls"
-  #Coveralls.wear!
-  SimpleCov.command_name "pry-test"
-  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
-  SimpleCov.start do
-    add_filter "test/demos"
-  end
-end
-
 require_relative "../lib/pry-test"
